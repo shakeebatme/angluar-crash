95c85fc53e04ef63f2c8e3ec9b38af54358bf37b
Shakeeb Ahmed <shakeebahmed@MacBook-Pro.local>
2021-07-05 13:49:21 -0400

    init

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+    .deleteTask(task)
+    .subscribe(
+      () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+    );
+  }
+
+}
